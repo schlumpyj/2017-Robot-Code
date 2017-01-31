32d112fd33bed902411c00ce178b1b4bbc54b8b5
Joshua <bb20basketball@gmail.com>
2017-01-30 21:11:44 -0600

    Added multi-threading

--- Vision/GRIPstarter.py
+++ Vision/GRIPstarter.py
@@ -1,14 +1,15 @@
 import cv2 as cv
 import numpy as np
 import grip
+from GRIPget import WebcamVideoStream
 import time
-vid1 = cv.VideoCapture(0)
+vid1 = WebcamVideoStream(src=0)
 another = grip.GripPipeline()
 counter = 0
 start = time.time()
 try:
     while True:
-        _, img1 = vid1.read()
+        img1 = vid1.read()
         another.process(img1)
         counter+=1
         k = cv.waitKey(5) & 0xFF
