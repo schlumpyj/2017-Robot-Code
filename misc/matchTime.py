f0c06dfe7b73cab059aaf1878aa46aaa1814fc36
Joshua <bb20basketball@gmail.com>
2017-02-05 15:00:36 -0600

    Create matchTime.py

---
+++ misc/matchTime.py
@@ -0,0 +1,23 @@
+import wpilib
+from networktables import NetworkTables
+class MatchTime(object):
+
+    def __init__(self, timer, table):
+
+        self.timer = timer
+        self.startTime = 0
+        self.table = table
+
+    def startMode(self, isAuto=False):
+
+        self.isAuto = isAuto
+        self.startTime = self.timer.getFPGATimestamp()
+
+    def pushTime(self):
+
+        self.endTime = self.timer.getFPGATimestamp()-self.startTime
+
+        if self.isAuto:
+            self.endTime+=15.0
+
+        self.table.putNumber("TIME", self.endTime)
