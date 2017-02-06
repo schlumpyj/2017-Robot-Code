e94ea770cf2c395297695e623d46db89f47b1a36
cboy116 <calebjwiebolt@gmail.com>
2017-02-06 16:40:18 -0600

    Fixed stupid Github indent crap and invalid syntax errors

--- misc/driveStraight.py
+++ misc/driveStraight.py
@@ -3,70 +3,34 @@
 
 
 class driveStraight(object):
-  #Main class
-  def __init__(self,Timer,WhichMethod,Vibrator,FirstTime,Joystick,drive):
-    
-    self.timer = Timer #main timer 
-    self.whichMethod = WhichMethod #whatever this does?
-    self.vibrationClass = Vibrator #the instatiated vibration class form the parent file
-    self.thereticalButtonPress = false
-    self.firstTime = FirstTime #set first time varible
-    self.joystick = Joystick #the joystick
-    self.Drive = drive #drive passed
-    
-    self.rotationXbox = 0
-    
-  def setWhichVariable(self,var):
-    self.whichMethod = var #update
-    
-  def setFirstTimeVariable(self,var):
-    self.firstTime = var
-    
-  def PressButton(self):
-    self.thereticalButtonPress = true
-    
-  def driveStraight(self):
+    def __init__(self,Timer,WhichMethod,Vibrator,FirstTime,Joystick,drive):
 
-    #On provided button down down
-    if self.thereticalButtonPress = true:
-      #set to false
-      self.thereticalButtonPress = false
+        self.timer = Timer #main timer 
+        self.whichMethod = WhichMethod #whatever this does?
+        self.vibrationClass = Vibrator #the instatiated vibration class form the parent file
+        self.thereticalButtonPress = False
+        self.firstTime = FirstTime #set first time varible
+        self.joystick = Joystick #the joystick
+        self.Drive = drive #drive passed
 
-      self.whichMethod = not self.whichMethod
-      if self.whichMethod:
-        self.vibrator.start(2)
-      else:
-        self.vibrator.start(1)
-      self.firstTime = True
-      
-    self.rotationXbox = (self.joystick.getRawAxis(4))*.5
+        self.rotationXbox = 0
 
-        """
-        This toggles between PID control and manual control
-        """
-        if self.whichMethod:
-            if self.rotationXbox < .15 and self.rotationXbox > -.15 and self.firstTime:
-                if self.timer.hasPeriodPassed(.5):
-                    self.Drive.updateSetpoint()
-                    self.firstTime = False
-            elif self.rotationXbox < .15 and self.rotationXbox > -.15 and not self.firstTime:
-                self.Drive.setPIDenable(True)
-            else:
-                self.timer.reset()
-                self.Drive.setPIDenable(True)
-                self.firstTime = True
-        else:
-            if self.rotationXbox < .15 and self.rotationXbox > -.15:
-    self.rotationXbox=0
-    
-"""
-old code:
+    def setWhichVariable(self,var):
+        self.whichMethod = var #update
 
- def driveStraight(self):
-        
-            Drive Straight Algorithm to allow mecanums to fly free
-        
-        if self.controlSwitch.get():
+    def setFirstTimeVariable(self,var):
+        self.firstTime = var
+
+    def PressButton(self):
+        self.thereticalButtonPress = True
+
+    def driveStraight(self):
+
+        #On provided button down down
+        if self.thereticalButtonPress = True:
+            #set to false
+            self.thereticalButtonPress = False
+
             self.whichMethod = not self.whichMethod
             if self.whichMethod:
                 self.vibrator.start(2)
@@ -74,11 +38,11 @@ old code:
                 self.vibrator.start(1)
             self.firstTime = True
 
- self.rotationXbox = (self.joystick.getRawAxis(4))*.5
+        self.rotationXbox = (self.joystick.getRawAxis(4))*.5
 
-        
+        """
         This toggles between PID control and manual control
-        
+        """
         if self.whichMethod:
             if self.rotationXbox < .15 and self.rotationXbox > -.15 and self.firstTime:
                 if self.timer.hasPeriodPassed(.5):
@@ -88,9 +52,9 @@ old code:
                 self.Drive.setPIDenable(True)
             else:
                 self.timer.reset()
-                self.Drive.setPIDenable(True)
+                self.Drive.setPIDenable(False)
                 self.firstTime = True
         else:
             if self.rotationXbox < .15 and self.rotationXbox > -.15:
-self.rotationXbox=0
-"""
+                self.rotationXbox=0
+            self.Drive.setPIDenable(False)
