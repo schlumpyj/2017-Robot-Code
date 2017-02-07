516da7e00a4a30ad85ee303cde091e470ceb409a
Joshua <bb20basketball@gmail.com>
2017-02-06 20:37:27 -0600

    Moved somethings around

--- misc/driveStraight.py
+++ misc/driveStraight.py
@@ -3,28 +3,29 @@
 
 
 class driveStraight(object):
-    def __init__(self,Timer,WhichMethod,Vibrator,FirstTime,Joystick,drive):
+    
+    def __init__(self, timer, whichMethod, vibrator, firstTime, joystick, drive):
 
-        self.timer = Timer #main timer 
-        self.whichMethod = WhichMethod #whatever this does?
-        self.vibrationClass = Vibrator #the instatiated vibration class form the parent file
+        self.timer = timer #main timer 
+        self.whichMethod = whichMethod #whatever this does?
+        self.vibrationClass = vibrator #the instatiated vibration class form the parent file
         self.thereticalButtonPress = False
-        self.firstTime = FirstTime #set first time varible
-        self.joystick = Joystick #the joystick
+        self.firstTime = firstTime #set first time varible
+        self.joystick = joystick #the joystick
         self.Drive = drive #drive passed
 
         self.rotationXbox = 0
 
-    def setWhichVariable(self,var):
+    def setWhichVariable(self, var):
         self.whichMethod = var #update
 
-    def setFirstTimeVariable(self,var):
+    def setFirstTimeVariable(self, var):
         self.firstTime = var
 
     def PressButton(self):
         self.thereticalButtonPress = True
 
-    def driveStraight(self):
+    def driveStraight(self, rotationValue):
 
         #On provided button down down
         if self.thereticalButtonPress = True:
@@ -38,23 +39,20 @@ class driveStraight(object):
                 self.vibrator.start(1)
             self.firstTime = True
 
-        self.rotationXbox = (self.joystick.getRawAxis(4))*.5
-
         """
         This toggles between PID control and manual control
         """
         if self.whichMethod:
-            if self.rotationXbox < .15 and self.rotationXbox > -.15 and self.firstTime:
+            if rotationValue < .15 and rotationValue > -.15 and self.firstTime:
                 if self.timer.hasPeriodPassed(.5):
                     self.Drive.updateSetpoint()
                     self.firstTime = False
-            elif self.rotationXbox < .15 and self.rotationXbox > -.15 and not self.firstTime:
+            elif rotationValue < .15 and rotationValue > -.15 and not self.firstTime:
                 self.Drive.setPIDenable(True)
             else:
                 self.timer.reset()
                 self.Drive.setPIDenable(False)
                 self.firstTime = True
         else:
-            if self.rotationXbox < .15 and self.rotationXbox > -.15:
-                self.rotationXbox=0
+
             self.Drive.setPIDenable(False)
