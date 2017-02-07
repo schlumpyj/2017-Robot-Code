5f1ddb8c2c885ece5ac5729287eba85f0d567062
Joshua <bb20basketball@gmail.com>
2017-02-06 20:44:09 -0600

    Added stop state

--- autonomous/drive-straight.py
+++ autonomous/drive-straight.py
@@ -1,4 +1,4 @@
-from robotpy_ext.autonomous import StatefulAutonomous, timed_state
+from robotpy_ext.autonomous import StatefulAutonomous, timed_state, state
 
 class DriveForward(StatefulAutonomous):
 
@@ -6,12 +6,16 @@ class DriveForward(StatefulAutonomous):
 
     def initialize(self):
 
-        self.speed = .4
+        self.speed = -.4
 
     @timed_state(duration=0.5, next_state='drive_forward', first=True)
     def drive_wait(self):
         self.drive.tankMove(0,0,0)
 
-    @timed_state(duration=2)
+    @timed_state(duration=2, next_state='stop')
     def drive_forward(self):
         self.drive.tankMove(0, 1, self.speed)
+    
+    @state()
+    def stop(self):
+        self.drive.tankMove(0,0,0)
