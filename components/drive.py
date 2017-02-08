6891397370f92f3d79425d26906e8c3a7be74ef9
Joshua <bb20basketball@gmail.com>
2017-02-07 16:24:59 -0800

    Update drive.py

--- components/drive.py
+++ components/drive.py
@@ -27,13 +27,13 @@ class Drive(object):
         self.turnController = turnController
 
 
-        visionP = 0.05 #Likely will have to be much higher
+        visionP = 0.01 #Likely will have to be much higher
 
         visionController = wpilib.PIDController(visionP, 0, 0, 0, lambda: self.vision_x, output=self.autoAlignOutput)
         visionController.setInputRange(0.0, 320.0)
         visionController.setOutputRange(-.5, .5)
         visionController.setContinuous(False)
-        visionController.setPercentTolerance(2)
+        visionController.setPercentTolerance(4)
         self.visionController = visionController
         self.visionController.setSetpoint(160.0)
 
@@ -41,9 +41,9 @@ class Drive(object):
 
         autoTurn = wpilib.PIDController(visionP, 0, 0, 0, self.gyro, output=self.autoTurnOutput)
         autoTurn.setInputRange(-180.0,  180.0)
-        autoTurn.setOutputRange(-.5, .5)
+        autoTurn.setOutputRange(-.25, .25)
         autoTurn.setContinuous(True)
-        autoTurn.setPercentTolerance(2)
+        autoTurn.setPercentTolerance(3)
         self.autoTurn = autoTurn
 
 
@@ -51,16 +51,19 @@ class Drive(object):
 
         if not self.turnController.isEnable():
             if rotation < .15 and rotation > -.15:
- -              self.rotation = 0
- -          else:
- -              self.rotation = rotation
+                 self.rotation = 0
+            else:
+                 self.rotation = rotation
             self.rotation = rotation
 
         if self.autoTurn.isEnable():
             self.rotation = self.rotateAuto
 
+        if not self.visionController.isEnable():
+            self.strafe = throttle*x
+
         self.drivePiston.set(wpilib.DoubleSolenoid.Value.kForward)
-        self.robotDrive.mecanumDrive_Cartesian(throttle*x, self.rotation, throttle*y, 0)
+        self.robotDrive.mecanumDrive_Cartesian(self.strafe, -1*self.rotation, throttle*y, 0)
 
     def tankMove(self, x, y, throttle):
 
@@ -68,7 +71,7 @@ class Drive(object):
         self.robotDrive.arcadeDrive(throttle*y, throttle*x, True)
 
     def updateSetpoint(self, controller, angle=0):
-        if controller == "turn":
+        if controller == "teleop":
             self.turnController.setSetpoint(self.gyro.getYaw())
         elif controller == "auto":
             self.autoTurn.setSetpoint(angle)
@@ -88,10 +91,18 @@ class Drive(object):
             self.turnController.disable()
             self.rotation = 0
 
-    def engageVisionX(self, value):
+    def engageVisionX(self, state, value):
+        if state:
+            if self.visionController.isEnable():
+                return True
+            self.visionController.enable()
+            self.vision_x = value
+            print (self.vision_x)
+        else:
+            self.visionController.disable()
 
-        self.visionController.enable()
-        self.vision_x = value
+    def disableVision(self):
+        self.visionController.disable()
 
     def pidWrite(self, output):
 
